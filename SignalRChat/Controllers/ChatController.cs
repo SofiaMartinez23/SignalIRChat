8aab6641b9db57240aa71e66acb35095a66df213
SofiaMartinez23 <sofiamg2392001@gmail.com>
2025-02-26 21:50:56 +0100

    Add project files.

---
+++ SignalRChat/Controllers/ChatController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatSignalIr.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, string> listaChats =
+            new Dictionary<int, string>()
+        {
+            {1, "GrupoClase" },
+            {2, "GrupoVacaciones" },
+            {3, "GrupoFamilia" },
+            {4, "GrupoPueblo" },
+            {5, "GrupoPiscina" }
+        };
+
+        public IActionResult ListaChats()
+        {
+            return View(listaChats);
+        }
+
+        public IActionResult Chat(int idChat)
+        {
+            return View("Chat", idChat);
+        }
+    }
+}
